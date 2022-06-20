bc991d5358bf9fc728e9396c3e70146695d25cb1
archeoss <archeosAnacritis@gmail.com>
2022-06-20 22:27:44 +0300

    repo fix

--- lab_01/build/CMakeFiles/lab_01_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lab_01_autogen"
-  "lab_01_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lab_01_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- lab_01/build/CMakeFiles/main_library_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/main_library_autogen"
-  "main_library_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/main_library_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
